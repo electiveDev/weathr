3ad138901f5b53839341db79ae1487a2f02281d3
electiveDev <git@buck.ovh>
2026-08-15 10:42:38 +0200

    Add seasonal world and tree variations

--- src/scene/world/decorations.rs
+++ src/scene/world/decorations.rs
@@ -3,13 +3,26 @@ use crate::scene::world::style::{TreeAppearance, WorldSceneStyle};
 use std::io;
 
 const TREE_ASCII: &str = include_str!("assets/tree.txt");
+const SPRING_TREE_ASCII: &str = include_str!("assets/tree_spring.txt");
+const AUTUMN_TREE_ASCII: &str = include_str!("assets/tree_autumn.txt");
+const WINTER_TREE_ASCII: &str = include_str!("assets/tree_winter.txt");
 const FENCE_ASCII: &str = include_str!("assets/fence.txt");
 const MAILBOX_ASCII: &str = include_str!("assets/mailbox.txt");
 const PINE_TREE_ASCII: &str = include_str!("assets/pine_tree.txt");
-const BARE_TREE_ASCII: &str =
-    "       |       \n      /|\\      \n       |       \n       |       \n     __|__";
-const TREE_ACCENT_SPOTS: [(u16, u16); 7] =
-    [(7, 0), (5, 1), (10, 1), (4, 2), (11, 2), (7, 3), (10, 3)];
+const SPRING_BLOSSOM_SPOTS: [(u16, u16); 7] =
+    [(7, 0), (4, 1), (10, 1), (3, 2), (12, 2), (6, 3), (10, 4)];
+const AUTUMN_LEAF_SPOTS: [(u16, u16); 7] = [(7, 0), (4, 1), (9, 1), (2, 2), (9, 2), (6, 3), (4, 4)];
+const SUMMER_FRUIT_SPOTS: [(u16, u16); 9] = [
+    (10, 1),
+    (15, 2),
+    (7, 3),
+    (13, 3),
+    (18, 4),
+    (5, 5),
+    (14, 5),
+    (10, 6),
+    (17, 6),
+];
 
 pub struct Decorations;
 
@@ -48,28 +61,47 @@ impl Decorations {
         if tree_x == 0 {
             return Ok(());
         }
-        let (tree_ascii, accent) = match style.tree_appearance {
-            TreeAppearance::Bare => (BARE_TREE_ASCII, None),
-            TreeAppearance::Blossoming | TreeAppearance::Autumn => (TREE_ASCII, Some('*')),
-            TreeAppearance::Full => (TREE_ASCII, None),
+        let tree_ascii = match style.tree_appearance {
+            TreeAppearance::Bare => WINTER_TREE_ASCII,
+            TreeAppearance::Blossoming => SPRING_TREE_ASCII,
+            TreeAppearance::Autumn => AUTUMN_TREE_ASCII,
+            TreeAppearance::Full => TREE_ASCII,
         };
         let line_count = tree_ascii.lines().count() as u16;
         let tree_y = layout.horizon_y.saturating_sub(line_count);
         render_art(renderer, tree_ascii, tree_x, tree_y, style.tree_foliage)?;
 
-        if let Some(symbol) = accent {
-            for (index, (offset_x, offset_y)) in TREE_ACCENT_SPOTS.iter().enumerate() {
-                renderer.render_char(
-                    tree_x.saturating_add(*offset_x),
-                    tree_y.saturating_add(*offset_y),
-                    symbol,
-                    style.flower_colors[index % style.flower_colors.len()],
-                )?;
-            }
+        match style.tree_appearance {
+            TreeAppearance::Blossoming => Self::render_tree_accents(
+                renderer,
+                tree_x,
+                tree_y,
+                '*',
+                &SPRING_BLOSSOM_SPOTS,
+                &style.flower_colors,
+            )?,
+            TreeAppearance::Autumn => Self::render_tree_accents(
+                renderer,
+                tree_x,
+                tree_y,
+                '*',
+                &AUTUMN_LEAF_SPOTS,
+                &style.flower_colors,
+            )?,
+            TreeAppearance::Full => Self::render_tree_accents(
+                renderer,
+                tree_x,
+                tree_y,
+                'o',
+                &SUMMER_FRUIT_SPOTS,
+                &style.fruit_colors,
+            )?,
+            TreeAppearance::Bare => {}
         }
 
         Ok(())
     }
+
     fn render_fence(
         &self,
         renderer: &mut TerminalRenderer,
@@ -83,6 +115,24 @@ impl Decorations {
         let line_count = FENCE_ASCII.lines().count() as u16;
         let fence_y = layout.horizon_y.saturating_sub(line_count);
         render_art(renderer, FENCE_ASCII, fence_x, fence_y, style.fence)
+    }
+    fn render_tree_accents(
+        renderer: &mut TerminalRenderer,
+        tree_x: u16,
+        tree_y: u16,
+        symbol: char,
+        spots: &[(u16, u16)],
+        colors: &[crossterm::style::Color],
+    ) -> io::Result<()> {
+        for (index, (offset_x, offset_y)) in spots.iter().enumerate() {
+            renderer.render_char(
+                tree_x.saturating_add(*offset_x),
+                tree_y.saturating_add(*offset_y),
+                symbol,
+                colors[index % colors.len()],
+            )?;
+        }
+        Ok(())
     }
 
     fn render_mailbox(
@@ -140,4 +190,41 @@ fn render_art(
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_spots_are_filled(tree: &str, spots: &[(u16, u16)]) {
+        for &(x, y) in spots {
+            let character = tree
+                .lines()
+                .nth(y as usize)
+                .and_then(|line| line.chars().nth(x as usize));
+            assert!(
+                matches!(character, Some(character) if character != ' '),
+                "tree accent spot ({x}, {y}) must be inside the tree"
+            );
+        }
+    }
+
+    #[test]
+    fn seasonal_tree_assets_have_distinct_growth_stages() {
+        assert_eq!(SPRING_TREE_ASCII.lines().count(), 8);
+        assert_eq!(TREE_ASCII.lines().count(), 10);
+        assert_eq!(AUTUMN_TREE_ASCII.lines().count(), 8);
+        assert_eq!(WINTER_TREE_ASCII.lines().count(), 8);
+        assert!(TREE_ASCII.lines().count() > SPRING_TREE_ASCII.lines().count());
+        assert!(SPRING_TREE_ASCII.contains("###"));
+        assert!(AUTUMN_TREE_ASCII.contains("  "));
+        assert!(WINTER_TREE_ASCII.contains('|'));
+    }
+
+    #[test]
+    fn seasonal_tree_accents_fit_their_assets() {
+        assert_spots_are_filled(SPRING_TREE_ASCII, &SPRING_BLOSSOM_SPOTS);
+        assert_spots_are_filled(TREE_ASCII, &SUMMER_FRUIT_SPOTS);
+        assert_spots_are_filled(AUTUMN_TREE_ASCII, &AUTUMN_LEAF_SPOTS);
+    }
 }
--- src/scene/world/style.rs
+++ src/scene/world/style.rs
@@ -23,6 +23,7 @@ pub struct WorldSceneStyle {
     pub soil: Color,
     pub tree_foliage: Color,
     pub tree_appearance: TreeAppearance,
+    pub fruit_colors: [Color; 3],
     pub fence: Color,
     pub mailbox: Color,
     pub water: Color,
@@ -54,6 +55,15 @@ impl WorldSceneStyle {
                 },
                 tree_foliage: Color::DarkGreen,
                 tree_appearance: TreeAppearance::Full,
+                fruit_colors: [
+                    Color::Red,
+                    Color::Yellow,
+                    Color::Rgb {
+                        r: 210,
+                        g: 60,
+                        b: 35,
+                    },
+                ],
                 fence: Color::White,
                 mailbox: Color::Blue,
                 water: Color::Blue,
@@ -90,6 +100,15 @@ impl WorldSceneStyle {
                 },
                 tree_foliage: Color::Rgb { r: 0, g: 50, b: 0 },
                 tree_appearance: TreeAppearance::Full,
+                fruit_colors: [
+                    Color::DarkRed,
+                    Color::DarkYellow,
+                    Color::Rgb {
+                        r: 130,
+                        g: 45,
+                        b: 25,
+                    },
+                ],
                 fence: Color::Grey,
                 mailbox: Color::DarkBlue,
                 water: Color::DarkBlue,
@@ -115,6 +134,21 @@ impl WorldSceneStyle {
                         g: 130,
                         b: 55,
                     };
+                    self.soil = Color::Rgb {
+                        r: 82,
+                        g: 68,
+                        b: 45,
+                    };
+                    self.water = Color::Rgb {
+                        r: 40,
+                        g: 135,
+                        b: 190,
+                    };
+                    self.water_edge = Color::Rgb {
+                        r: 80,
+                        g: 190,
+                        b: 180,
+                    };
                     self.flower_colors = [Color::Magenta, Color::Red, Color::Yellow, Color::White];
                     self.tree_foliage = Color::Green;
                 } else {
@@ -124,6 +158,13 @@ impl WorldSceneStyle {
                         g: 70,
                         b: 25,
                     };
+                    self.soil = Color::Rgb {
+                        r: 42,
+                        g: 36,
+                        b: 28,
+                    };
+                    self.water = Color::DarkBlue;
+                    self.water_edge = Color::Blue;
                     self.flower_colors = [
                         Color::DarkMagenta,
                         Color::DarkRed,
@@ -139,11 +180,47 @@ impl WorldSceneStyle {
                 if is_day {
                     self.grass_primary = Color::Green;
                     self.grass_secondary = Color::DarkGreen;
+                    self.soil = Color::Rgb {
+                        r: 128,
+                        g: 94,
+                        b: 48,
+                    };
+                    self.water = Color::Rgb {
+                        r: 30,
+                        g: 135,
+                        b: 205,
+                    };
+                    self.water_edge = Color::Cyan;
                     self.tree_foliage = Color::Green;
+                    self.fruit_colors = [
+                        Color::Red,
+                        Color::Yellow,
+                        Color::Rgb {
+                            r: 210,
+                            g: 60,
+                            b: 35,
+                        },
+                    ];
                 } else {
                     self.grass_primary = Color::DarkGreen;
                     self.grass_secondary = Color::Rgb { r: 0, g: 40, b: 0 };
+                    self.soil = Color::Rgb {
+                        r: 60,
+                        g: 45,
+                        b: 25,
+                    };
+                    self.water = Color::DarkBlue;
+                    self.water_edge = Color::Blue;
                     self.tree_foliage = Color::Rgb { r: 0, g: 50, b: 0 };
+                    self.fruit_colors = [
+                        Color::DarkRed,
+                        Color::DarkYellow,
+                        Color::Rgb {
+                            r: 130,
+                            g: 45,
+                            b: 25,
+                        },
+                    ];
                 }
                 self.flower_chance = 3;
                 self.tree_appearance = TreeAppearance::Full;
@@ -155,6 +232,21 @@ impl WorldSceneStyle {
                         r: 139,
                         g: 69,
                         b: 19,
+                    };
+                    self.soil = Color::Rgb {
+                        r: 112,
+                        g: 70,
+                        b: 34,
+                    };
+                    self.water = Color::Rgb {
+                        r: 35,
+                        g: 115,
+                        b: 165,
+                    };
+                    self.water_edge = Color::Rgb {
+                        r: 70,
+                        g: 160,
+                        b: 160,
                     };
                     self.flower_colors = [
                         Color::DarkRed,
@@ -182,6 +274,13 @@ impl WorldSceneStyle {
                         g: 40,
                         b: 10,
                     };
+                    self.soil = Color::Rgb {
+                        r: 62,
+                        g: 40,
+                        b: 22,
+                    };
+                    self.water = Color::DarkBlue;
+                    self.water_edge = Color::Blue;
                     self.flower_colors = [
                         Color::DarkRed,
                         Color::DarkYellow,
@@ -202,20 +301,70 @@ impl WorldSceneStyle {
                 self.tree_appearance = TreeAppearance::Autumn;
             }
             Season::Winter => {
-                self.flower_chance = 0;
-                self.tree_foliage = if is_day {
-                    Color::Rgb {
+                if is_day {
+                    self.grass_primary = Color::Rgb {
+                        r: 145,
+                        g: 155,
+                        b: 125,
+                    };
+                    self.grass_secondary = Color::Rgb {
+                        r: 105,
+                        g: 115,
+                        b: 95,
+                    };
+                    self.soil = Color::Rgb {
+                        r: 135,
+                        g: 130,
+                        b: 115,
+                    };
+                    self.water = Color::Rgb {
+                        r: 90,
+                        g: 130,
+                        b: 155,
+                    };
+                    self.water_edge = Color::Rgb {
+                        r: 165,
+                        g: 190,
+                        b: 195,
+                    };
+                    self.tree_foliage = Color::Rgb {
                         r: 100,
                         g: 70,
                         b: 45,
-                    }
+                    };
                 } else {
-                    Color::Rgb {
+                    self.grass_primary = Color::Rgb {
+                        r: 55,
+                        g: 65,
+                        b: 55,
+                    };
+                    self.grass_secondary = Color::Rgb {
+                        r: 40,
+                        g: 48,
+                        b: 40,
+                    };
+                    self.soil = Color::Rgb {
+                        r: 65,
+                        g: 65,
+                        b: 60,
+                    };
+                    self.water = Color::Rgb {
+                        r: 55,
+                        g: 80,
+                        b: 100,
+                    };
+                    self.water_edge = Color::Rgb {
+                        r: 100,
+                        g: 125,
+                        b: 135,
+                    };
+                    self.tree_foliage = Color::Rgb {
                         r: 55,
                         g: 40,
                         b: 30,
-                    }
-                };
+                    };
+                }
+                self.flower_chance = 0;
                 self.tree_appearance = TreeAppearance::Bare;
             }
         }
@@ -254,15 +403,40 @@ mod tests {
         let spring = style_for(Season::Spring);
         assert_eq!(spring.tree_appearance, TreeAppearance::Blossoming);
         assert_eq!(spring.flower_chance, 18);
+        assert_eq!(
+            spring.soil,
+            Color::Rgb {
+                r: 82,
+                g: 68,
+                b: 45
+            }
+        );
 
         let summer = style_for(Season::Summer);
         assert_eq!(summer.tree_appearance, TreeAppearance::Full);
         assert_eq!(summer.flower_chance, 3);
+        assert_eq!(
+            summer.soil,
+            Color::Rgb {
+                r: 128,
+                g: 94,
+                b: 48
+            }
+        );
+        assert_eq!(summer.fruit_colors[0], Color::Red);
 
         let autumn = style_for(Season::Autumn);
         assert_eq!(autumn.tree_appearance, TreeAppearance::Autumn);
         assert_eq!(autumn.flower_chance, 2);
         assert!(autumn.flower_chance < summer.flower_chance);
+        assert_eq!(
+            autumn.soil,
+            Color::Rgb {
+                r: 112,
+                g: 70,
+                b: 34
+            }
+        );
         assert_eq!(
             autumn.flower_colors,
             [
@@ -284,9 +458,66 @@ mod tests {
         let winter = style_for(Season::Winter);
         assert_eq!(winter.tree_appearance, TreeAppearance::Bare);
         assert_eq!(winter.flower_chance, 0);
-        assert_eq!(winter.grass_primary, DEFAULT_PALETTE.ground_day);
+        assert_eq!(
+            winter.grass_primary,
+            Color::Rgb {
+                r: 145,
+                g: 155,
+                b: 125,
+            }
+        );
+        assert_eq!(
+            winter.soil,
+            Color::Rgb {
+                r: 135,
+                g: 130,
+                b: 115,
+            }
+        );
+        assert_eq!(
+            winter.water_edge,
+            Color::Rgb {
+                r: 165,
+                g: 190,
+                b: 195,
+            }
+        );
+    }
+
+    #[test]
+    fn seasonal_ground_and_night_styles_are_distinct() {
+        let day_styles = [
+            style_for(Season::Spring),
+            style_for(Season::Summer),
+            style_for(Season::Autumn),
+            style_for(Season::Winter),
+        ];
+        assert_ne!(day_styles[0].soil, day_styles[1].soil);
+        assert_ne!(day_styles[1].soil, day_styles[2].soil);
+        assert_ne!(day_styles[2].soil, day_styles[3].soil);
+        assert_ne!(day_styles[0].water_edge, day_styles[3].water_edge);
+
         let spring_night = style_for_time(Season::Spring, false);
         assert_eq!(spring_night.grass_primary, Color::DarkGreen);
         assert_eq!(spring_night.tree_foliage, Color::Rgb { r: 0, g: 50, b: 0 });
+        assert_eq!(
+            spring_night.soil,
+            Color::Rgb {
+                r: 42,
+                g: 36,
+                b: 28
+            }
+        );
+
+        let winter_night = style_for_time(Season::Winter, false);
+        assert_eq!(
+            winter_night.water,
+            Color::Rgb {
+                r: 55,
+                g: 80,
+                b: 100,
+            }
+        );
+        assert_ne!(spring_night.soil, winter_night.soil);
     }
 }
